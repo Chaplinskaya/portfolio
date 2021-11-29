ecff1d6169bc8a9b9015ed9945a5e9f893c1024f
Tatyana <chaplinskaya.tatyana@gmail.com>
2021-11-29 11:25:08 +0200

    add script menu

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,15 +1,21 @@
 $(document).ready(function() {
     const hamburger = document.querySelector('.hamburger'),
           menu = document.querySelector('.menu'),
-          closeElem = document.querySelector('.menu__close');
+          closeElem = document.querySelector('.menu__close'),
+          menulink = document.querySelectorAll('.menu__link');
     
     hamburger.addEventListener('click', () => {
-    menu.classList.add('active');
+        menu.classList.add('active');
     });
     closeElem.addEventListener('click', () => {
         menu.classList.remove('active');
     });
-
+    menulink.forEach(item => {
+        item.classList.add('active');
+        item.addEventListener('click', () => {
+            menu.classList.remove('active');
+        });
+    });
     const counters = document.querySelectorAll('.skills__ratings-counter'),
         lines = document.querySelectorAll('.skills__ratings-line span');
 
