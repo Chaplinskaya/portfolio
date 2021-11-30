812d0d9669d3fc5e948ec2a794f5baa3dd1a281f
Tatyana <chaplinskaya.tatyana@gmail.com>
2021-11-30 11:16:28 +0200

    edit skills

--- src/mailer/smart.php
+++ src/mailer/smart.php
@@ -14,12 +14,12 @@ $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
 $mail->Username = 'моя почта';                 // Наш логин
-$mail->Password = 'Мой пароль';                           // Наш пароль от ящика
+$mail->Password = 'мой пароль';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
 $mail->setFrom('моя почта', 'My portfolio');   // От кого письмо 
-$mail->addAddress('временный пароль');     // Add a recipient
+$mail->addAddress('временная почта');     // Add a recipient
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
